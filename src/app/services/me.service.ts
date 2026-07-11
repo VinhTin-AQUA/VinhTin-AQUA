e7f60c190622b6cefb23ef02909ebdd28b56410c
VinhTin-AQUA <tinhovinh@gmail.com>
2026-07-11 17:39:26 +0700

    update

--- src/app/services/me.service.ts
+++ src/app/services/me.service.ts
@@ -6,7 +6,7 @@ import { ThemeService } from './theme.service';
     providedIn: 'root',
 })
 export class MeService {
-    private themeService = inject(ThemeService); 
+    private themeService = inject(ThemeService);
 
     me = signal<Me>({
         name: 'Ho Vinh Tin',
@@ -14,19 +14,32 @@ export class MeService {
         contacts: [
             { name: 'Email', icon: 'email', link: 'mailto:tinhovinh@gmail.com' },
             { name: 'Github', icon: 'github', link: 'https://github.com/VinhTin-AQUA' },
-            { name: 'LinkedIn', icon: 'linkedin', link: 'https://www.linkedin.com/in/tin-ho-vinh/' },
+            {
+                name: 'LinkedIn',
+                icon: 'linkedin',
+                link: 'https://www.linkedin.com/in/tin-ho-vinh/',
+            },
             { name: 'CV', icon: 'cv', link: '' },
         ],
         openToWork: '✦ available',
         avatar: './avatar.png',
-        avatartWithSunglass: './avatartWithSunglass.png'
+        avatartWithSunglass: './avatartWithSunglass.png',
     });
+
+    constructor() {
+        const me = this.me();
+
+        const images = [me.avatar, me.avatartWithSunglass];
+
+        images.forEach((src) => {
+            const img = new Image();
+            img.src = src;
+        });
+    }
 
     currentAvatar = computed(() => {
         const me = this.me();
 
-        return this.themeService.currentTheme() === 'light'
-            ? me.avatartWithSunglass
-            : me.avatar;
+        return this.themeService.currentTheme() === 'light' ? me.avatartWithSunglass : me.avatar;
     });
 }
